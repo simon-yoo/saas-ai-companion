26f09356f8ec78a1567f536c575a88cf4777e688
Simon Yoo <yoosghn0@gmail.com>
2023-11-15 10:46:39 +1300

    auth layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='flex justify-center items-center h-full'>{children}</div>
+  )
+}
+
+export default layout
