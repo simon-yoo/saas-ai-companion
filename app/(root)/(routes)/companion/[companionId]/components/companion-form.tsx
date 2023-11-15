cc2642143c080b8a8ed2338ad3ca970f28362c93
Simon Yoo <yoosghn0@gmail.com>
2023-11-16 11:48:05 +1300

    fixing error

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -1,3 +1,15 @@
-export const CompanionForm = () => {
+'use client'
+
+import { Category, Companion } from '@prisma/client'
+
+interface CompanionFormProps {
+  categories: Category[]
+  initialData: Companion | null
+}
+
+export const CompanionForm = ({
+  categories,
+  initialData,
+}: CompanionFormProps) => {
   return <div>companion</div>
 }
