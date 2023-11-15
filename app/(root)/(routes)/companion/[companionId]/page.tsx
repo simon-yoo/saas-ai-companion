b2ae15e606c92503937c97f97cd6c7df8f5667a9
Simon Yoo <yoosghn0@gmail.com>
2023-11-16 11:34:41 +1300

    import companionform

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,5 +1,22 @@
-const CompanionIdPage = () => {
-  return <div>page</div>
+import prismadb from '@/lib/prismadb'
+import { CompanionForm } from './components/companion-form'
+
+interface CompanionIdPageProps {
+  params: {
+    companionId: string
+  }
+}
+
+const CompanionIdPage = async ({ params }: CompanionIdPageProps) => {
+  // TODO: Check subscription
+  const companion = await prismadb.companion.findUnique({
+    where: {
+      id: params.companionId,
+    },
+  })
+
+  const categories = await prismadb.category.findMany()
+  return <CompanionForm initialData={companion} categories={categories} />
 }
 
 export default CompanionIdPage
