fcd799d72bd6bb269c6722e21f3b5759404720c5
Simon Yoo <yoosghn0@gmail.com>
2023-11-16 11:03:32 +1300

    create Categories component

---
+++ components/categories.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Category } from '@prisma/client'
+
+interface CategorisProps {
+  data: Category[]
+}
+
+const Categories = () => {
+  return <div>categories</div>
+}
+
+export default Categories
