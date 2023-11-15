3407ff5e0bf7d6a54ef323f765e7ade252f06eef
Simon Yoo <yoosghn0@gmail.com>
2023-11-16 11:07:44 +1300

    create categoty buttons

--- components/categories.tsx
+++ components/categories.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { cn } from '@/lib/utils'
 import { Category } from '@prisma/client'
 
 interface CategorisProps {
@@ -7,7 +8,27 @@ interface CategorisProps {
 }
 
 const Categories = ({ data }: CategorisProps) => {
-  return <div>categories</div>
+  return (
+    <div className='w-full overflow-x-auto space-x-2 flex p-1'>
+      <button
+        className={cn(
+          `flex items-center text-center text-xs md:text-sm px-2 md:px-4 py-2 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transition`
+        )}
+      >
+        Newest
+      </button>
+      {data.map((item) => (
+        <button
+          key={item.id}
+          className={cn(
+            `flex items-center text-center text-xs md:text-sm px-2 md:px-4 py-2 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transition`
+          )}
+        >
+          {item.name}
+        </button>
+      ))}
+    </div>
+  )
 }
 
 export default Categories
