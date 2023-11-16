8b6af05917cb1e53eb37cc020e83ee948327f250
Simon Yoo <yoosghn0@gmail.com>
2023-11-16 19:55:15 +1300

    props

--- components/chat-form.tsx
+++ components/chat-form.tsx
@@ -1,6 +1,25 @@
 'use client'
 
-const ChatForm = () => {
+import { ChatRequestOptions } from 'ai'
+import { ChangeEvent, FormEvent } from 'react'
+
+interface ChatFormProps {
+  isLoading: boolean
+  input: string
+  handleInputChange: (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
+  ) => void
+  onSubmit: (
+    e: FormEvent<HTMLFormElement>,
+    chatRequestOPtions?: ChatRequestOptions | undefined
+  ) => void
+}
+const ChatForm = ({
+  isLoading,
+  input,
+  handleInputChange,
+  onSubmit,
+}: ChatFormProps) => {
   return <div>chat-form</div>
 }
 
