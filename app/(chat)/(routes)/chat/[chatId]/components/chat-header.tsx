7833b2d10392004d39f75bc6a8d3bab0df6ae197
Simon Yoo <yoosghn0@gmail.com>
2023-11-16 19:03:08 +1300

    chatheader ing

--- app/(chat)/(routes)/chat/[chatId]/components/chat-header.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/chat-header.tsx
@@ -1,6 +1,9 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
 import { Companion, Message } from '@prisma/client'
+import { ChevronLeft } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 
 interface ChatHeaderProps {
   companion: Companion & {
@@ -11,7 +14,16 @@ interface ChatHeaderProps {
   }
 }
 const ChatHeader = ({ companion }: ChatHeaderProps) => {
-  return <div>chat-header</div>
+  const router = useRouter()
+  return (
+    <div className='flex w-full justify-between items-center border-b border-primary/10 pb-4'>
+      <div className='flex gap-x-2 items-center'>
+        <Button onClick={() => router.back()} size='icon' variant='ghost'>
+          <ChevronLeft className='h-8 w-8' />
+        </Button>
+      </div>
+    </div>
+  )
 }
 
 export default ChatHeader
