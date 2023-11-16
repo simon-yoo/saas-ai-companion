a843758e38afa3c5aed9ff45632277d284c1f434
Simon Yoo <yoosghn0@gmail.com>
2023-11-16 19:35:11 +1300

    fixing error

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,5 +1,6 @@
 import prismadb from '@/lib/prismadb'
 import { CompanionForm } from './components/companion-form'
+import { auth, redirectToSignIn } from '@clerk/nextjs'
 
 interface CompanionIdPageProps {
   params: {
@@ -8,10 +9,18 @@ interface CompanionIdPageProps {
 }
 
 const CompanionIdPage = async ({ params }: CompanionIdPageProps) => {
+  const { userId } = auth()
+
   // TODO: Check subscription
+
+  if (!userId) {
+    return redirectToSignIn()
+  }
+
   const companion = await prismadb.companion.findUnique({
     where: {
       id: params.companionId,
+      userId,
     },
   })
 
