4c7d3658c8c336ea9dafdc256f9aef156b9aac94
Simon Yoo <yoosghn0@gmail.com>
2023-11-16 18:27:27 +1300

    props

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -2,7 +2,14 @@ import Categories from '@/components/categories'
 import SearchInput from '@/components/search-input'
 import prismadb from '@/lib/prismadb'
 
-const page = async () => {
+interface RootPageProps {
+  searchParams: {
+    categoryId: string
+    name: string
+  }
+}
+
+const page = async ({ searchParams }: RootPageProps) => {
   const categories = await prismadb.category.findMany()
   return (
     <div className='h-full p-4 space-y-2'>
