a1be696f5cd51f588b8d5fce077906f49a108e7b
Simon Yoo <yoosghn0@gmail.com>
2023-11-16 18:46:34 +1300

    companions

--- components/companions.tsx
+++ components/companions.tsx
@@ -1,7 +1,8 @@
 import { Companion } from '@prisma/client'
 import Image from 'next/image'
-import { Card, CardHeader } from '@/components/ui/card'
+import { Card, CardFooter, CardHeader } from '@/components/ui/card'
 import Link from 'next/link'
+import { MessageSquare, MessagesSquare } from 'lucide-react'
 
 interface CompanionsProps {
   data: (Companion & {
@@ -39,7 +40,16 @@ const Companions = ({ data }: CompanionsProps) => {
                   alt='Companion'
                 />
               </div>
+              <p className='font-bold'>{item.name}</p>
+              <p className='text-xs'>{item.description}</p>
             </CardHeader>
+            <CardFooter className='flex items-center justify-between text-xs text-muted-foreground'>
+              <p className='lowercase'>@{item.userName}</p>
+              <div className='flex items-center'>
+                <MessagesSquare className='w-3 h-3 mr-1' />
+                {item._count.messages}
+              </div>
+            </CardFooter>
           </Link>
         </Card>
       ))}
