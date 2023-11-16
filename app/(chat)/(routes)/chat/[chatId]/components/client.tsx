9cff66bea05bae9fadac40c77fa100b8b6541b1f
Simon Yoo <yoosghn0@gmail.com>
2023-11-17 12:56:01 +1300

    import chatmessages component

--- app/(chat)/(routes)/chat/[chatId]/components/client.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/client.tsx
@@ -7,6 +7,7 @@ import ChatHeader from './chat-header'
 import { useRouter } from 'next/navigation'
 import { FormEvent, useState } from 'react'
 import ChatForm from '@/components/chat-form'
+import { ChatMessages } from '@/components/chat-messages'
 
 interface ChatClientProps {
   companion: Companion & {
@@ -50,7 +51,11 @@ const ChatClient = ({ companion }: ChatClientProps) => {
   return (
     <div className='flex flex-col h-full p-4 space-y-2'>
       <ChatHeader companion={companion} />
-      <div>Messages TODO</div>
+      <ChatMessages
+        companion={companion}
+        isLoading={isLoading}
+        messages={messages}
+      />
       <ChatForm
         isLoading={isLoading}
         input={input}
