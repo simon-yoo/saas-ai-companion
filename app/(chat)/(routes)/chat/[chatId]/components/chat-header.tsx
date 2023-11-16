3b5765c7482b0feca088ab33e44d2acb9120ce1c
Simon Yoo <yoosghn0@gmail.com>
2023-11-16 19:13:08 +1300

    dropdown to edit or delete

--- app/(chat)/(routes)/chat/[chatId]/components/chat-header.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/chat-header.tsx
@@ -2,9 +2,22 @@
 
 import { Button } from '@/components/ui/button'
 import { Companion, Message } from '@prisma/client'
-import { ChevronLeft } from 'lucide-react'
+import {
+  ChevronLeft,
+  Edit,
+  MessagesSquare,
+  MoreVertical,
+  Trash,
+} from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { BotAvatar } from './bot-avatar'
+import { useUser } from '@clerk/nextjs'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 
 interface ChatHeaderProps {
   companion: Companion & {
@@ -16,6 +29,8 @@ interface ChatHeaderProps {
 }
 const ChatHeader = ({ companion }: ChatHeaderProps) => {
   const router = useRouter()
+  const { user } = useUser()
+
   return (
     <div className='flex w-full justify-between items-center border-b border-primary/10 pb-4'>
       <div className='flex gap-x-2 items-center'>
@@ -23,7 +38,38 @@ const ChatHeader = ({ companion }: ChatHeaderProps) => {
           <ChevronLeft className='h-8 w-8' />
         </Button>
         <BotAvatar src={companion.src} />
+        <div className='flex flex-col gap-y-1'>
+          <div className='flex items-center gap-x-2'>
+            <p className='font-bold'>{companion.name}</p>
+            <div className='flex items-center text-xs text-muted-foreground'>
+              <MessagesSquare className='w-3 h-3 mr-1' />
+              {companion._count.messages}
+            </div>
+          </div>
+          <p className='text-xs text-muted-foreground'>
+            Create by {companion.userName}
+          </p>
+        </div>
       </div>
+      {user?.id === companion.userId && (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant='secondary' size='icon'>
+              <MoreVertical />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end'>
+            <DropdownMenuItem>
+              <Edit className='w-4 h-4 mr-2' />
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Trash className='w-4 h-4 mr-2' />
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )}
     </div>
   )
 }
