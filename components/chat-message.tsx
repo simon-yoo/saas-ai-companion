19a26896d0d0e8708feed56ec7a316f737191b15
Simon Yoo <yoosghn0@gmail.com>
2023-11-17 13:04:02 +1300

    chat message

--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -1,5 +1,31 @@
 import React from 'react'
+import { useToast } from '@/components/ui/use-toast'
+import { useTheme } from 'next-themes'
+export interface ChatMessageProps {
+  role: 'system' | 'user'
+  content?: string
+  isLoading: boolean
+  src?: string
+}
 
-export const ChatMessage = () => {
+export const ChatMessage = ({
+  role,
+  content,
+  isLoading,
+  src,
+}: ChatMessageProps) => {
+  const { toast } = useToast()
+  const { theme } = useTheme()
+
+  const onCopu = () => {
+    if (!content) {
+      return
+    }
+
+    navigator.clipboard.writeText(content)
+    toast({
+      description: 'Messages copied to clipboard',
+    })
+  }
   return <div>ChatMessage</div>
 }
