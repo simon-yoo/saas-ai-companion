780e137a1a3545c3186dbf31a044fd48cc6107d5
Simon Yoo <yoosghn0@gmail.com>
2023-11-17 18:24:47 +1300

    fixing error

--- app/(chat)/(routes)/chat/[chatId]/components/chat-header.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/chat-header.tsx
@@ -10,7 +10,7 @@ import {
   Trash,
 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
-import { BotAvatar } from './bot-avatar'
+import { BotAvatar } from '../../../../../../components/bot-avatar'
 import { useUser } from '@clerk/nextjs'
 import {
   DropdownMenu,
