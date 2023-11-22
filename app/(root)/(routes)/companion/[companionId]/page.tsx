5d7d3124890bd62eb131f7f43ca1a803e1e67982
Simon Yoo <yoosghn0@gmail.com>
2023-11-22 15:59:02 +1300

    fix error

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,5 +1,5 @@
 import prismadb from '@/lib/prismadb'
-import { CompanionForm } from './components/companion-form'
+import { CompanionForm } from './components/CompanionForm'
 import { auth, redirectToSignIn } from '@clerk/nextjs'
 
 interface CompanionIdPageProps {
