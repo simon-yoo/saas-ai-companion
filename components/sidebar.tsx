0c845f1c85df6e1135dfaecd3f0a7e296ef6c3e3
Simon Yoo <yoosghn0@gmail.com>
2023-11-22 15:53:54 +1300

    fixing errors

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
   const pathname = usePathname()
   const router = useRouter()
 
-  const route = [
+  const routes = [
     {
       icon: Home,
       href: '/',
@@ -24,13 +24,14 @@ const Sidebar = () => {
     {
       icon: Settings,
       href: '/settings',
-      label: 'Setting',
+      label: 'Settings',
       pro: false,
     },
   ]
 
   const onNavigate = (url: string, pro: boolean) => {
-    // TODO: Check if PRO
+    //TODO: check if pro
+
     return router.push(url)
   }
 
@@ -38,10 +39,10 @@ const Sidebar = () => {
     <div className='space-y-4 flex flex-col h-full text-primary bg-secondary'>
       <div className='p-3 flex flex-1 justify-center'>
         <div className='space-y-2'>
-          {route.map((route) => (
+          {routes.map((route) => (
             <div
-              onClick={() => onNavigate(route.href, route.pro)}
               key={route.href}
+              onClick={() => onNavigate(route.href, route.pro)}
               className={cn(
                 'text-muted-foreground text-xs group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-primary hover:bg-primary/10 rounded-lg transition',
                 pathname === route.href && 'bg-primary/10 text-primary'
