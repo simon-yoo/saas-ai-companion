f344ae16216f9483582acc149c1cc822322e8830
Simon Yoo <yoosghn0@gmail.com>
2023-11-22 15:45:34 +1300

    rename

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,6 +1,6 @@
+import SearchInput from '@/components/SearchInput'
 import Categories from '@/components/categories'
 import Companions from '@/components/companions'
-import SearchInput from '@/components/search-input'
 import prismadb from '@/lib/prismadb'
 
 interface RootPageProps {
