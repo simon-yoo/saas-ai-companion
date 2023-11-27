0a644af969b5b771d7dad35206378de925d70d97
Simon Yoo <yoosghn0@gmail.com>
2023-11-27 13:52:15 +1300

    loading

--- components/ChatMessages.tsx
+++ components/ChatMessages.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Companion } from '@prisma/client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ChatMessage, ChatMessageProps } from '@/components/ChatMessage'
 
 interface ChatMessagesProps {
@@ -14,10 +14,24 @@ export const ChatMessages = ({
   messages = [],
   companion,
 }: ChatMessagesProps) => {
+  const [fakeLoading, setFakeLoading] = useState(
+    messages.length === 0 ? true : false
+  )
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setFakeLoading(false)
+    }, 1000)
+
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [])
+
   return (
     <div className='flex-1 overflow-y-auto pr-4'>
       <ChatMessage
-        isLoading={isLoading}
+        isLoading={fakeLoading}
         src={companion.src}
         role='system'
         content={`Hello, I am ${companion.name}, ${companion.description}`}
