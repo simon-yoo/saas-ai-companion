a460cc66c279375805ea5ddefbde8409525100ac
Simon Yoo <yoosghn0@gmail.com>
2023-11-29 09:33:32 +1300

    chatid route

---
+++ app/api/chat/[chatId]/route.ts
@@ -0,0 +1,8 @@
+import { StreamingTextResponse, LangChainStream } from 'ai'
+import { auth, currentUser } from '@clerk/nextjs'
+import { CallbackManager } from 'langchain/callbacks'
+import { NextResponse } from 'next/server'
+
+import { MemoryManager } from '@/lib/memory'
+import { rateLimit } from '@/lib/rate-limit'
+import prismadb from '@/lib/prismadb'
