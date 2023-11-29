8c52f64a4a23bc2d4411646d6ff418bf5df66d40
Simon Yoo <yoosghn0@gmail.com>
2023-11-29 15:34:24 +1300

    ispro sidebar create

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,11 +4,12 @@ import { checkSubscription } from '@/lib/subscription'
 
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
   const isPro = await checkSubscription()
+
   return (
     <div className='h-full'>
       <Navbar isPro={isPro} />
       <div className='hidden md:flex mt-16 w-20 flex-col fixed inset-y-0'>
-        <Sidebar />
+        <Sidebar isPro={isPro} />
       </div>
       <main className='md:pl-20 pt-16 h-full'>{children}</main>
     </div>
