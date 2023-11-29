3317f292207b58b8ef09d28d13e27f3a37c926ab
Simon Yoo <yoosghn0@gmail.com>
2023-11-30 11:00:26 +1300

    fix

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,20 @@
+'use client'
+import { Menu } from 'lucide-react'
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+// @ts-ignore
+import Sidebar from '@/components/sidebar'
+
+const MobileSidebar = ({ isPro }: { isPro: boolean }) => {
+  return (
+    <Sheet>
+      <SheetTrigger className='md:hidden pr-4'>
+        <Menu />
+      </SheetTrigger>
+      <SheetContent side='left' className='p-0 bg-secondary pt-10 w-32'>
+        <Sidebar isPro={isPro} />
+      </SheetContent>
+    </Sheet>
+  )
+}
+
+export default MobileSidebar
