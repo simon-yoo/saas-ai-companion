e18f5cff4531abd3932834ada9eba3ad7df83930
Simon Yoo <yoosghn0@gmail.com>
2023-11-30 11:00:15 +1300

    fix

---
+++ components/chat-form.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { ChatRequestOptions } from 'ai'
+import { ChangeEvent, FormEvent } from 'react'
+import { Input } from '@/components/ui/input'
+import { Button } from './ui/button'
+import { SendHorizonal } from 'lucide-react'
+
+interface ChatFormProps {
+  isLoading: boolean
+  input: string
+  handleInputChange: (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
+  ) => void
+  onSubmit: (
+    e: FormEvent<HTMLFormElement>,
+    chatRequestOPtions?: ChatRequestOptions | undefined
+  ) => void
+}
+const ChatForm = ({
+  isLoading,
+  input,
+  handleInputChange,
+  onSubmit,
+}: ChatFormProps) => {
+  return (
+    <form
+      onSubmit={onSubmit}
+      className='border-t border-primary/10 py-4 flex items-center gap-x-2'
+    >
+      <Input
+        disabled={isLoading}
+        value={input}
+        onChange={handleInputChange}
+        placeholder='Type a message'
+        className='rounded-lg bg-primary/10'
+      />
+      <Button disabled={isLoading} variant='ghost' />
+      <SendHorizonal className='h-6 w-6' />
+    </form>
+  )
+}
+
+export default ChatForm
