4a8ee9f89e277d894f94ee6002c51a09910919e3
Simon Yoo <yoosghn0@gmail.com>
2023-11-30 11:00:06 +1300

    fix

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,7 +1,7 @@
-import SearchInput from '@/components/SearchInput'
-import prismadb from '@/lib/prismadb'
-import Companions from '@/components/Companions'
 import Categories from '@/components/Categories'
+import Companions from '@/components/Companions'
+import SearchInput from '@/components/search-input'
+import prismadb from '@/lib/prismadb'
 
 interface RootPageProps {
   searchParams: {
