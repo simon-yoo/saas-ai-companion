52974c88c9ac16e872eade659c5484be2749bc8b
Simon Yoo <yoosghn0@gmail.com>
2023-11-29 15:43:49 +1300

    subscription butotn

--- components/SubscriptionButton.tsx
+++ components/SubscriptionButton.tsx
@@ -1,16 +1,19 @@
 'use client'
 
-import { Button } from "./ui/button"
+import { Sparkle } from 'lucide-react'
+import { Button } from './ui/button'
 
 interface SubscriptionButtonProps {
   isPro: boolean
 }
 
-const SubscriptionButton = ({ isPro= false }: SubscriptionButtonProps) => {
-  return <Button size='sm' variant={isPro ? 'default' : 'premium'}>
-    {isPro ? 'Manage Subscription' : 'Upgrade'}
-    {!isPro? }
-  </Button>
+const SubscriptionButton = ({ isPro = false }: SubscriptionButtonProps) => {
+  return (
+    <Button size='sm' variant={isPro ? 'default' : 'premium'}>
+      {isPro ? 'Manage Subscription' : 'Upgrade'}
+      {!isPro && <Sparkle className='h-4 w-4 ml-2 fill-white' />}
+    </Button>
+  )
 }
 
 export default SubscriptionButton
