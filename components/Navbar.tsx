49de01ee1c140a654575587e7ae55cc54fb00e47
Simon Yoo <yoosghn0@gmail.com>
2023-11-29 13:35:12 +1300

    promodal hook

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,6 +9,7 @@ import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
 import { ModeToggle } from './ModeToggle'
 import MobileSidebar from '@/components/MobileSidebar'
+import { usePropModal } from '@/hooks/use-pro-modal'
 
 const font = Poppins({
   weight: '600',
@@ -16,6 +17,8 @@ const font = Poppins({
 })
 
 const Navbar = () => {
+  const proModal = usePropModal()
+
   return (
     <div className='fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16'>
       <div className='flex items-center'>
@@ -32,7 +35,7 @@ const Navbar = () => {
         </Link>
       </div>
       <div className='flex items-center gap-x-3'>
-        <Button variant='premium' size='sm'>
+        <Button onClick={proModal.onOpen} variant='premium' size='sm'>
           Upgrade
           <Sparkles className='h-4 w-4 fill-white text-white ml-2' />
         </Button>
