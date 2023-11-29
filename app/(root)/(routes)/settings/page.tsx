262a88bbea26e80b080d7f07b449429fd4af530c
Simon Yoo <yoosghn0@gmail.com>
2023-11-29 15:43:02 +1300

    settings page

---
+++ app/(root)/(routes)/settings/page.tsx
@@ -0,0 +1,19 @@
+import { checkSubscription } from '@/lib/subscription'
+import React from 'react'
+
+const page = async () => {
+  const isPro = await checkSubscription()
+
+  return (
+    <div className='h-full p-4 space-y-2'>
+      <h3 className='text-lg font-medium'>Settings</h3>
+      <div className=' text-muted-foreground text-sm'>
+        {isPro
+          ? 'You are currently on a Pro plan.'
+          : 'You are currently on a fee plan.'}
+      </div>
+    </div>
+  )
+}
+
+export default page
